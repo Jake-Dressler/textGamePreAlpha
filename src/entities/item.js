04fba7b3f67961a9103850bb0b064084a67b9d09
Jake-Dressler <jakedressler.jd@yahoo.com>
2025-01-18 14:02:51 -0500

    Update item.js

--- src/entities/item.js
+++ src/entities/item.js
@@ -1,10 +1,16 @@
 export class Item {
 
     name;
+    description;
     effect;
     price;
+    weight;
 
-
-
-
+    constructor(name, description, effect, price, weight){
+        this.name = name;
+        this.description = description;
+        this.effect = effect;
+        this.price = price;
+        this.weight = weight;
+    }
 }
